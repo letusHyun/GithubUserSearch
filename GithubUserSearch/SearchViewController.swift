2b5d1f0a043d551a21e297fa2c1874c5a6825665
seokhyun <letushyun@gmail.com>
2023-01-02 02:27:11 +0900

    first commit

---
+++ GithubUserSearch/SearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchViewController.swift
+//  GithubUserSearch
+//
+//  Created by SeokHyun on 2023/01/02.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    
+  }
+}
